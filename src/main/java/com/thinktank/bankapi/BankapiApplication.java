a61c284c32b1ec4c5901864ef602466fbc558dc7
GarbyX <60491335+GarbyX@users.noreply.github.com>
2021-11-08 03:00:40 +0100

    Add files via upload

---
+++ src/main/java/com/thinktank/bankapi/BankapiApplication.java
@@ -0,0 +1,13 @@
+package com.thinktank.bankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinktank/bankapi/BankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinktank.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
